85b098a6cebe8bbb612f9c2b57131ec77177dcf7
kyogo <apple@appurunoMacBook-Pro.local>
2020-01-05 13:22:18 +0900

    update example

--- example.js
+++ example.js
@@ -1,10 +1,10 @@
 // inputに入力データ全体が入る
 function Main(input) {
 	lines = input.split("\n");
-	tmp = lines[1].split(" ");
+	words = lines[1].split(" ");
 	var a = Number(lines[0]);
-	var b = Number(tmp[0]);
-	var c = Number(tmp[1]);
+	var b = Number(words[0]);
+	var c = Number(words[1]);
 	var s = lines[2];
 	console.log('%d %s',a+b+c,s);
 }
