4c716380e03665c3999f60512496d0d0f435e5c3
kyogo <apple@appurunoMacBook-Pro.local>
2020-01-10 21:45:07 +0900

    A - 500 Yen Coins

--- practice/AtCoderBeginnerContest150/abc150_a.js
+++ practice/AtCoderBeginnerContest150/abc150_a.js
@@ -2,11 +2,9 @@
 function Main(input) {
 	var lines = input.split("\n");
 	var words = lines[0].split(" ");
-	var a = Number(lines[0]);
-	var b = Number(words[0]);
-	var c = Number(words[1]);
-	var s = lines[2];
-	console.log('%d %s',a+b+c,s);
+	var k = Number(words[0]);
+	var x = Number(words[1])
+	console.log(k * 500 >= x ? 'Yes' : 'No')
 }
 
 /*
@@ -16,7 +14,5 @@ Main(require("fs").readFileSync("/dev/stdin", "utf8"));
 
 // テスト用の標準入力
 Main(`
-1
-2 3
-test
+4 2000
 `.replace('\n', ''))
