3a9188cc3329917e1585e7d46c608b596ac81c8a
kyogo <apple@appurunoMacBook-Pro.local>
2020-01-11 23:44:56 +0900

    B - 文字列大好きいろはちゃんイージー / Iroha Loves Strings

---
+++ practice/AtCoderBeginnerContest42/abc042_b.js
@@ -0,0 +1,19 @@
+// inputに入力データ全体が入る
+function Main(input) {
+	var lines = input.split("\n");
+	var s = lines.splice(1, lines.length)
+	var s_sorted = s.sort((a, b) => a > b ? 1 : -1)
+	console.log(s_sorted.join(''))
+}
+
+/*
+提出時はコメントアウトを外す
+Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+*/
+
+// テスト用の標準入力
+Main(`
+3 3
+dxx
+axx
+cxx`.replace('\n', ''))
