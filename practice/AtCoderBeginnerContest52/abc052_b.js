1daaaa275feaa7b00acb685fa990470a62cafb2d
kyogo <apple@appurunoMacBook-Pro.local>
2020-02-05 14:44:53 +0900

    A - ABC/ARC

--- practice/AtCoderBeginnerContest52/abc052_b.js
+++ practice/AtCoderBeginnerContest52/abc052_b.js
@@ -1,23 +1,11 @@
 'use strict'
 const main = arg => {
 	const input = arg.trim().split("\n");
-	let N = input[0].split(' ')[0]
-	let S = input[1].split(' ')[0]
-	let Xs = []
-	let x = 0
-	Xs.push(x)
-	for(let i = 0; i < N; i++) {
-		if(S[i] === 'D') {
-			Xs.push(--x)
-		} else {
-			Xs.push(++x)
-		}
-	}
-	console.log(Math.max(...Xs))
+	let N = Number(input[0].split(' ')[0])
+	console.log(N >= 1200 ? 'ARC' : 'ABC')
 }   
 // main(require('fs').readFileSync('/dev/stdin', 'utf8'));
 
 main(`
-7
-DDIDDII
+1000
 `)
